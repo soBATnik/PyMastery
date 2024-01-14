ad40aab0f3a77376cfd1b992aea9c43e53cebfcd
Yurii <yurii.askinderov@gmail.com>
2024-01-15 00:41:07 +0200

    Homework_1

--- homework_1/hw1_2.py
+++ homework_1/hw1_2.py
@@ -1,6 +1,7 @@
 user_string = input('Input yur string: ')
 
-new_user_string = user_string.strip(" ")
+new_user_string = user_string.strip(" ")    #Без этой строки пробелы перед первым и после последнего слова будут считаться за слова
+
 words_count = new_user_string.count(' ') + 1
 
 print(words_count)
--- homework_1/hw1_7.py
+++ homework_1/hw1_7.py
@@ -3,7 +3,5 @@ user_input = input("Enter your string: ")
 new_string = ""
 for i in range(len(user_input)):
     if i % 3:
-        new_string = new_string + user_input[i]
+        new_string += user_input[i]
 print(new_string)
-
-
